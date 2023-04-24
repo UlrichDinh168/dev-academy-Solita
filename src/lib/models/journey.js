a7349b48302951a136247eac23f0b1e2af3dfdfb
Ulrich Dinh <duong0028@gmail.com>
2023-04-24 13:32:42 +0300

    add mongoose model

---
+++ src/lib/models/journey.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const journeySchema = mongoose.Schema({
+  Departure: {
+    type: String,
+    default: undefined
+  },
+  Return: {
+    type: String,
+    default: undefined
+  },
+  "Departure station id": {
+    type: String,
+    default: undefined
+  },
+  "Departure station name": {
+    type: String,
+    default: undefined
+  },
+  "Return station id": {
+    type: String,
+    default: undefined
+  },
+  "Return station name": {
+    type: String,
+    default: undefined
+  },
+  "Covered distance (m)": {
+    type: Number,
+    default: undefined
+  },
+  "Duration (sec)": {
+    type: Number,
+    default: undefined
+  }
+})
+
+export default mongoose.model("station", journeySchema);
