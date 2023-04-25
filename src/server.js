fa1533ed6a689bbc96ac4b3e3d92d33fc84fa1c5
Ulrich Dinh <duong0028@gmail.com>
2023-04-25 10:09:42 +0300

    server.js backbone

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-// const path = require('path');
-import path from 'path'
-// const express = require('express');
-// const router = require('./api/router');
+import createError from 'http-errors';
 import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors';
 import router from './api/routes/router.js'
 import { fileURLToPath } from 'url';
 
@@ -10,11 +11,17 @@ const { PORT = 3001 } = process.env;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log(__dirname, '__dirname');
+
 const app = express();
+
 
 // Middleware that parses json and looks at requests where the Content-Type header matches the type option.
 app.use(express.json());
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 
 // Serve API requests from the router
 app.use('/api', router);
@@ -23,7 +30,20 @@ app.use('/api', router);
 // Serve app production bundle
 app.use(express.static('dist/app'));
 const root = path.join(__dirname, 'app/index.html')
-// Handle client routing, return all requests to the app
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 // if (process.env.NODE_ENV === 'production') {
 app.get('*', (_req, res) => {
@@ -35,3 +55,5 @@ app.get('*', (_req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
+
+export default app;
