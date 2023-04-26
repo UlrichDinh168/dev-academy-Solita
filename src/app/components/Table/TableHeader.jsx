d9d8a65524480fb48f64e87de75e2921abfe8e30
Ulrich Dinh <duong0028@gmail.com>
2023-04-26 15:58:47 +0300

    Add Table MUI and config

---
+++ src/app/components/Table/TableHeader.jsx
@@ -0,0 +1,42 @@
+import TableRow from '@mui/material/TableRow';
+import TableHead from '@mui/material/TableHead';
+import { visuallyHidden } from '@mui/utils';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Box from '@mui/material/Box';
+import TableCell from '@mui/material/TableCell';
+
+export default function TableHeader(props) {
+  const { order, orderBy, onRequestSort, headCells } =
+    props;
+  const createSortHandler = (newOrderBy) => (event) => {
+    onRequestSort(event, newOrderBy);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
---
+++ src/app/components/Table/index.jsx
@@ -0,0 +1,207 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TableRow from '@mui/material/TableRow';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TablePagination from '@mui/material/TablePagination';
+import Paper from '@mui/material/Paper';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+
+import TableHeader from './TableHeader';
+import { getComparator, stableSort } from './util'
+
+const DEFAULT_ORDER = 'asc';
+const DEFAULT_ORDER_BY = 'Duration (m)';
+const DEFAULT_ROWS_PER_PAGE = 5;
+const headCells = [
+  {
+    id: 'Departure station name',
+    numeric: false,
+    disablePadding: false,
+    label: 'Departure',
+  },
+  {
+    id: 'Return station name',
+    numeric: true,
+    disablePadding: false,
+    label: 'Destination',
+  },
+  {
+    id: 'Covered distance (m)',
+    numeric: true,
+    disablePadding: false,
+    label: 'Distance (km)',
+  },
+  {
+    id: 'Duration (sec)',
+    numeric: true,
+    disablePadding: false,
+    label: 'Duration (m)',
+  },
+]
+
+export default function EnhancedTable({ rows }) {
+  console.log(rows, 'rows');
+  const [order, setOrder] = React.useState(DEFAULT_ORDER);
+  const [orderBy, setOrderBy] = React.useState(DEFAULT_ORDER_BY);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [visibleRows, setVisibleRows] = React.useState(null);
+  const [rowsPerPage, setRowsPerPage] = React.useState(DEFAULT_ROWS_PER_PAGE);
+  const [paddingHeight, setPaddingHeight] = React.useState(0);
+
+  React.useEffect(() => {
+    let rowsOnMount = stableSort(
+      rows,
+      getComparator(DEFAULT_ORDER, DEFAULT_ORDER_BY),
+    );
+
+    rowsOnMount = rowsOnMount?.slice(
+      0 * DEFAULT_ROWS_PER_PAGE,
+      0 * DEFAULT_ROWS_PER_PAGE + DEFAULT_ROWS_PER_PAGE,
+    );
+
+    setVisibleRows(rowsOnMount);
+  }, [rows]);
+
+  const handleRequestSort = React.useCallback(
+    (event, newOrderBy) => {
+      const isAsc = orderBy === newOrderBy && order === 'asc';
+      const toggledOrder = isAsc ? 'desc' : 'asc';
+      setOrder(toggledOrder);
+      setOrderBy(newOrderBy);
+
+      const sortedRows = stableSort(rows, getComparator(toggledOrder, newOrderBy));
+      const updatedRows = sortedRows?.slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage,
+      );
+
+      setVisibleRows(updatedRows);
+    },
+    [order, orderBy, page, rowsPerPage, rows],
+  );
+
+  const handleChangePage = React.useCallback(
+    (event, newPage) => {
+      setPage(newPage);
+      const sortedRows = stableSort(rows, getComparator(order, orderBy));
+      const updatedRows = sortedRows?.slice(
+        newPage * rowsPerPage,
+        newPage * rowsPerPage + rowsPerPage,
+      );
+
+      setVisibleRows(updatedRows);
+
+      // Avoid a layout jump when reaching the last page with empty rows.
+      const numEmptyRows =
+        newPage > 0 ? Math.max(0, (1 + newPage) * rowsPerPage - rows.length) : 0;
+
+      const newPaddingHeight = (dense ? 33 : 53) * numEmptyRows;
+      setPaddingHeight(newPaddingHeight);
+      // nextJourneys(1)
+    },
+    [order, orderBy, dense, rowsPerPage, rows,],
+  );
+
+  const handleChangeRowsPerPage = React.useCallback(
+    (event) => {
+      const updatedRowsPerPage = parseInt(event.target.value, 10);
+      setRowsPerPage(updatedRowsPerPage);
+
+      setPage(0);
+
+      const sortedRows = stableSort(rows, getComparator(order, orderBy));
+      const updatedRows = sortedRows?.slice(
+        0 * updatedRowsPerPage,
+        0 * updatedRowsPerPage + updatedRowsPerPage,
+      );
+
+      setVisibleRows(updatedRows);
+
+      // There is no layout jump to handle on the first page.
+      setPaddingHeight(0);
+    },
+    [order, orderBy, rows],
+  );
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+          >
+            <TableHeader
+              order={order}
+              orderBy={orderBy}
+              headCells={headCells}
+              onRequestSort={handleRequestSort}
+              rowCount={rows?.length}
+            />
+            <TableBody>
+              {visibleRows
+                ? visibleRows.map((row, index) => {
+                  const labelId = `enhanced-table-checkbox-${index}`;
+                  return (
+                    <TableRow
+                      hover
+                      tabIndex={-1}
+                      key={index}
+                    // sx={{ cursor: 'pointer' }}
+                    >
+
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="normal"
+                      >
+                        {row['Departure station name']}
+                      </TableCell>
+                      <TableCell align="right">{row['Return station name']}</TableCell>
+                      <TableCell align="right">{row['Covered distance (m)']}</TableCell>
+                      <TableCell align="right">{row['Duration (sec)']}</TableCell>
+                    </TableRow>
+                  );
+                })
+                : null}
+              {paddingHeight > 0 && (
+                <TableRow
+                  style={{
+                    height: paddingHeight,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows?.length || 0}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      />
+    </Box>
+  );
+}
