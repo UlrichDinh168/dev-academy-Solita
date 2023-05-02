8fda558336a6cd90a581e4befd87364f01521212
Ulrich Dinh <duong0028@gmail.com>
2023-05-02 23:52:38 +0300

    update url

--- src/app/constant.js
+++ src/app/constant.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 // export const API_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:3001'
-export const API_URL = 'http://localhost:3001'
+// export const API_URL = 'http://localhost:3001'
+export const API_URL = 'http://ec2-44-211-78-150.compute-1.amazonaws.com:3001/'
 
 export const instance = axios.create({
   baseURL: API_URL,
