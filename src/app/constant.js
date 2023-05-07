a36bec308ac89f495f2a968e81e38a72af099e5b
Ulrich Dinh <duong0028@gmail.com>
2023-05-07 11:21:03 +0300

    change api_url to prod

--- src/app/constant.js
+++ src/app/constant.js
@@ -1,9 +1,10 @@
 import axios from "axios"
 
-export const API_URL = 'http://localhost:3001'
+export const REACT_APP_API_URL = 'http://localhost:3001'
+const REACT_APP_PROD_ENV = 'https://solita-dev-ulrich.herokuapp.com/'
 
 export const instance = axios.create({
-  baseURL: API_URL,
+  baseURL: process.env.NODE_ENV === 'production' ? REACT_APP_PROD_ENV : REACT_APP_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
